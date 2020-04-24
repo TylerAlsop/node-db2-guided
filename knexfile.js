115530dd18303ceb4b8fed749a562b67161ae2b7
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-23 18:36:09 -0600

    Setup the knexfile.js with a module.exports.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,7 @@
-// Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: "sqlite3", // Specifying the DBMS
+  useNullAsDefault: true, //  A flag that's required for SQLite specifically
+  connection: {
+    filename: "./data/produce.db3" // Location of our database file
   }
-
-};
+}
