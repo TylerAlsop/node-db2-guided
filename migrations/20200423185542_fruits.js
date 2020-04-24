b56d06dd1fe622fcd71f46d2b799a8c2a1da0896
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-23 19:03:15 -0600

    Setup the exports.up in the first fruits migration.

--- migrations/20200423185542_fruits.js
+++ migrations/20200423185542_fruits.js
@@ -1,8 +1,24 @@
+exports.up = async function(knex) {
+    await knex.schema.createTable("fruits", (table) => {
+        // table.integer("id").notNull().primary()
+        table.increments("id")
+        table.text("name").notNull().unique()
+        table.float("avgWeightOz").notNull()
+        table.boolean("delicious").notNull().defaultTo(true)
+    })
 
-exports.up = function(knex) {
-  
+
+    // CREATE TABLE IF NOT EXISTS "fruits" (
+    //     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    //     "name" TEXT NOT NULL UNIQUE,
+    //     "avgWeightOz" FLOAT NOT NULL,
+    //     "delicious" BOOLEAN NOT NULL DEFAULT true,
+    //     "color" TEXT
+    // );
+
+
 };
 
 exports.down = function(knex) {
-  
+
 };
